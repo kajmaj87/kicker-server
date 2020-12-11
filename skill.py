7ec83791233978fc1dd87906732651d4d0cb5a1e
kajman <kajmaj87@gmail.com>
2020-12-11 18:26:40 +0100

    Added multiple file handling

--- skill.py
+++ skill.py
@@ -1,25 +1,24 @@
 import csv
 import json
 import argparse
+import os
+
 from trueskill import rate, TrueSkill, Rating
 
 
 p = argparse.ArgumentParser(
     description="Converts raw csv input to a json with trueskill ratings"
 )
-p.add_argument("-i", "--input", help="input csv with raw scores", required=True)
+p.add_argument("files", help="input files in csv format with raw scores", nargs='*')
 p.add_argument("-o", "--output", help="output json with true skill ratings")
 
 config = p.parse_args()
 
-ranks = {}
-
-
-def rowToDict(row):
+def rowToDict(row, ranks):
     result = {}
     result["date"] = row[0]
-    result["groupA"] = {p: playerRank(p) for p in splitPlayers(row[1])}
-    result["groupB"] = {p: playerRank(p) for p in splitPlayers(row[2])}
+    result["groupA"] = {p: playerRank(p,ranks) for p in splitPlayers(row[1])}
+    result["groupB"] = {p: playerRank(p,ranks) for p in splitPlayers(row[2])}
     result["result"] = row[3]
     return result
 
@@ -28,25 +27,24 @@ def splitPlayers(group):
     return group.split("-")
 
 
-def playerRank(player):
-    global ranks
+def playerRank(player, ranks):
     if player not in ranks:
         ranks[player] = Rating()
     return ranks[player]
 
 
-def updateRanks(groups):
-    global ranks
+def updateRanks(groups, ranks):
     for group in groups:
         for player, ranking in group.items():
             ranks[player] = ranking
+    return ranks
 
 
-def processRanks(match):
+def processRanks(match, ranks):
     if match["result"] == "win":
-        updateRanks(rate([match["groupA"], match["groupB"]]))
+        return updateRanks(rate([match["groupA"], match["groupB"]]), ranks)
     if match["result"] == "loss":
-        updateRanks(rate([match["groupB"], match["groupA"]]))
+        return updateRanks(rate([match["groupB"], match["groupA"]]), ranks)
 
 
 def toRankObject(name, rating):
@@ -58,17 +56,31 @@ def toRankObject(name, rating):
     }
 
 
-with open(config.input, newline="") as datafile:
-    reader = csv.reader(datafile, delimiter=",")
-    # skip header row
-    next(reader, None)
-    matches = [processRanks(rowToDict(match)) for match in reader]
-    result = []
-    for k, v in sorted(ranks.items(), key=lambda item: 3 * item[1].sigma - item[1].mu):
-        result.append(toRankObject(k, v))
+def processOneCsv(csvPath):
+    with open(csvPath, newline="") as datafile:
+        reader = csv.reader(datafile, delimiter=",")
+        # skip header row
+        next(reader, None)
+        ranks = {}
 
-    if config.output is not None:
-        with open(config.output, "w") as outputjson:
-            json.dump({"rankings": result}, outputjson, indent=4)
-    else:
-        print(result)
+        for match in reader:
+            ranks = processRanks(rowToDict(match, ranks), ranks)
+
+        result = []
+        for k, v in sorted(ranks.items(), key=lambda item: 3 * item[1].sigma - item[1].mu):
+            result.append(toRankObject(k, v))
+        return result
+
+result = {}
+
+for f in config.files:
+    base = os.path.basename(f)
+    name = os.path.splitext(base)[0]
+    result[name] = processOneCsv(f)
+
+if config.output is not None:
+    with open(config.output, "w") as outputjson:
+        json.dump({"rankings": result}, outputjson, indent=4)
+else:
+    print(result)
+
